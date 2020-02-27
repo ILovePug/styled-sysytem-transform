214a2c55ac1fa506c2d91a152be4ff37c74d3eeb
ILovePug <wengqians@yahoo.com>
2020-02-27 08:16:36 -0500

    freshstart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { space } from 'styled-system'
+import  styled  from '@emotion/styled'
+
+const Box = styled.div`
+  text-align: center;
+  ${space}
+`;
+
+console.log(space)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box  p={4}>
+      hello
+    </Box>
   );
 }
 
